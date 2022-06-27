7f87b508b0bae271db246bbf3f7e8a91d2c73d90
Raghunath <raghunathmamidala12345@gmail.com>
2022-06-27 12:23:14 +0530

    make it better

--- mysql/urls.py
+++ mysql/urls.py
@@ -23,6 +23,4 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello', printHello),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
