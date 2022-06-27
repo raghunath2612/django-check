d9cd8765035b988ef57dae4906da35546caafedf
Raghunath <raghunathmamidala12345@gmail.com>
2022-06-27 11:39:00 +0530

    2

--- mysql/settings.py
+++ mysql/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vnnlo_4*^t*fzvq8)7d5dxn6b$tw^j=&96x@=wt$fifj%kqk+('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- mysql/urls.py
+++ mysql/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path
 
 from mysql.views import printHello
-
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello', printHello)
+    path('hello', printHello),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
